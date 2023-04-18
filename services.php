3a0a1d8e737bde34efdd6fecc97270d3a0bae60b
Maxence Guidez <202130087@collegealma.ca>
2023-04-18 19:05:46 -0400

    Add the services page.

--- services.php
+++ services.php
@@ -1,12 +1,25 @@
-
 <?php
 include("inclus/entete.inc.php");
 ?>
 <!-- SERVICES -->
-
-<img src="images/construction.jpeg" alt="En construction logo">
-<h2 class="mb-4">En construction...</h2>
-<a href="index.php">Accueil</a>
+<h2 class="mb-4">Nos Services</h2>
+<h6>Location de service</h6>
+<p>
+    Que ce soit pour un mariage, un bal de finissant
+    ou tout autre occasion, nos voitures font parti
+    des plus belles de la région.
+</p>
+<h6 class="mt-5">Location de chauffeur</h6>
+<p>
+    Le service de chauffer vous permet de vous déplacer
+    un peu partout dans la région sans vous soucier de la route.
+</p>
+<h6 class="mt-5">Photo</h6>
+<p>
+    Pour une occasion spécial ou pour impressionner
+    vos amis, un photographe professionnel peut vous
+    immortaliser avec la voiture de votre choix.
+</p>
 
 
 <?php
