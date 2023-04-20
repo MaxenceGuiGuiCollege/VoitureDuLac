23a1d2b9893596a06e705b9d28b71cd2a0cadf83
Maxence Guidez <202130087@collegealma.ca>
2023-04-20 17:54:35 -0400

    Part two, update the internationalization of prices and services page.

--- services.php
+++ services.php
@@ -12,12 +12,12 @@ include("inclus/entete.inc.php");
 ?>
 <!-- SERVICES -->
 <h2 class="mb-4"><?php echo $json['services_title'] ?></h2>
-<h6><?php echo $json['services_services'] ?></h6>
-<p><?php echo $json['services_services_text'] ?></p>
+<h6><?php echo $json['services_cars'] ?></h6>
+<p><?php echo $json['services_cars_text'] ?></p>
 <h6 class="mt-5"><?php echo $json['services_drivers'] ?></h6>
 <p><?php echo $json['services_drivers_text'] ?></p>
-<h6 class="mt-5"><?php echo $json['services_photo'] ?></h6>
-<p><?php echo $json['services_photo_text'] ?></p>
+<h6 class="mt-5"><?php echo $json['services_photos'] ?></h6>
+<p><?php echo $json['services_photos_text'] ?></p>
 <?php
 include("inclus/piedPage.inc.php");
 ?>
--- tarifs.php
+++ tarifs.php
@@ -1,42 +1,51 @@
 <?php
+include("librairies/fonctions.lib.php");
+//DEFINIR LA LANGUE
+if(isset($_GET['lang']))
+    $lang = $_GET['lang'];
+else if(isset($_COOKIE['lang']))
+    $lang = $_COOKIE['lang'];
+else
+    $lang = "fr";
+$json = obtenirJson($lang);
 include("inclus/entete.inc.php");
 ?>
 <!-- TARIFS -->
-<h2 class="mb-4">Nos Tarifs</h2>
+<h2 class="mb-4"><?php echo $json['prices_title'] ?></h2>
 <table class="table">
     <tr>
-        <th>Service</th>
-        <th>Prix</th>
+        <th><?php echo $json['prices_service'] ?></th>
+        <th><?php echo $json['prices_price'] ?></th>
     </tr>
     <tr>
-        <td rowspan="4">Voiture</td>
-        <td>100$/jour</td>
+        <td rowspan="4"><?php echo $json['prices_car'] ?></td>
+        <td>100$/<?php echo $json['prices_day'] ?></td>
     </tr>
     <tr>
-        <td>0$ si <= 100km</td>
+        <td>0$ <?php echo $json['prices_if'] ?> <= 100km</td>
     </tr>
     <tr>
-        <td>0.10$/km si > 100km</td>
+        <td>0.10$/km <?php echo $json['prices_if'] ?> > 100km</td>
     </tr>
     <tr>
-        <td>24.99$/jour Assurance (si besoin)</td>
+        <td>24.99$/<?php echo $json['prices_day'] ?> <?php echo $json['prices_insurance'] ?></td>
     </tr>
     <tr>
-        <td rowspan="2">Chauffeur</td>
-        <td>25$/heure (maximum 8 heures/jour)</td>
+        <td rowspan="2"><?php echo $json['prices_driver'] ?></td>
+        <td>25$/<?php echo $json['prices_hour'] ?> <?php echo $json['prices_maxtime'] ?></td>
     </tr>
     <tr>
-        <td>1000$/semaine</td>
+        <td>1000$/<?php echo $json['prices_week'] ?></td>
     </tr>
     <tr>
-        <td rowspan="3">Photo</td>
-        <td>50$/heure (Séance de photo incluant 5 photos numérique et une photo papier)</td>
+        <td rowspan="3"><?php echo $json['prices_photo'] ?></td>
+        <td>50$/<?php echo $json['prices_hour'] ?> <?php echo $json['prices_content_photoshoot'] ?></td>
     </tr>
     <tr>
-        <td>15$ Toutes les photos numérique</td>
+        <td>15$ <?php echo $json['prices_allphotos'] ?></td>
     </tr>
     <tr>
-        <td>7.50$ Une photo papier</td>
+        <td>7.50$ <?php echo $json['prices_paperphoto'] ?></td>
     </tr>
 </table>
 <?php
